73e6a4fe570f99a9924421212399ddf3fc76fb7a
Maxime STEVENOT <maxime.stevenot@manomano.com>
2023-12-01 10:58:44 +0100

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return input
+            .map { it.filter { c -> c.isDigit() } }
+            .sumOf { "${it.first()}${it.last()}".toLong() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { string ->
+            string.windowed(5, 1, true) {
+                when {
+                    it.first().isDigit() -> it.first().toString()
+                    it.startsWith("zero") -> "0"
+                    it.startsWith("one") -> "1"
+                    it.startsWith("two") -> "2"
+                    it.startsWith("three") -> "3"
+                    it.startsWith("four") -> "4"
+                    it.startsWith("five") -> "5"
+                    it.startsWith("six") -> "6"
+                    it.startsWith("seven") -> "7"
+                    it.startsWith("eight") -> "8"
+                    it.startsWith("nine") -> "9"
+                    else -> ""
+                }
+            }.filter { it.isNotEmpty() }
+        }.sumOf { "${it.first()}${it.last()}".toInt() }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
+    "----".println()
     part2(input).println()
 }
