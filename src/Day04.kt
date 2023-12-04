7e2f1e0e6bfd36d2dff654ff811d9cd2c211f544
Maxime STEVENOT <maxime.stevenot@manomano.com>
2023-12-04 10:45:11 +0100

    day4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -7,12 +7,22 @@ class Day04 : Day("04") {
         return parseGames(input)
             .map { it.first to parseNumbers(it.second) }
             .map { it.first to computeScore(it.second) }
-            .onEach { println(it) }
             .sumOf { it.second }
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        val games = parseGames(input).map { it.first to parseNumbers(it.second) }
+        val copies = IntArray(games.size) { 1 }
+        games.forEachIndexed { index, game ->
+            val count = getIntersectCount(game.second)
+            val range = (index + 1..index + count)
+            for (i in range) {
+                if (i < copies.size) {
+                    copies[i] += copies[index]
+                }
+            }
+        }
+        return copies.sum()
     }
 
     companion object {
@@ -20,10 +30,12 @@ class Day04 : Day("04") {
             .map { it.split(":") }
             .map { it[0].replace("Card", "").trim().toInt() to it[1].trim() }
 
-        private fun computeScore(numbers: List<List<Int>>) = numbers[0]
+        private fun computeScore(numbers: List<List<Int>>) = getIntersectCount(numbers)
+            .let { if (it > 0) 2.0.pow(it - 1.0).toInt() else 0 }
+
+        private fun getIntersectCount(numbers: List<List<Int>>) = numbers[0]
             .intersect(numbers[1].toSet())
             .count()
-            .let { if (it > 0) 2.0.pow(it - 1.0).toInt() else 0 }
 
         private fun parseNumbers(input: String) = input
             .split("|")
